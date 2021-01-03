6ddce86a5c0b29421967f5103a7b8b3afd819d4e
joyce <lostviolinist@gmail.com>
2021-01-03 18:00:32 +0800

    props, slots

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>{{title}}</h1>
+  <div v-if="showModal2">
+    <!-- pass in props -->
+    <!-- <Modal :header="header" :text="text" :sale="sale" @close="toggleModal"/> -->
+    <Modal :sale="sale" @close="toggleModal2">
+      <template v-slot:form>
+        <form>
+          <label for="fname">First name:</label><br>
+          <input type="text" id="fname" name="fname"><br>
+          <label for="lname">Last name:</label><br>
+          <input type="text" id="lname" name="lname">
+        </form>
+      </template>
+      <h1>Sale Giveaway!</h1>
+      <p>grab your free copy now</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModal2">open modal 2</button>
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from './components/Modal'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'my Vue App',
+      header: 'Sign up for the giveaway!',
+      text: 'Grab your free copy now',
+      sale: 'sale',
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
+    }
   }
 };
 </script>
@@ -22,5 +58,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #fff;
+  display: inline-block;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === sale }">
+            <slot></slot>
+            <div>
+                <slot name="form"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ["header", "text"],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    h1{
+        color: aqua;
+        border: none;
+    }
+
+    .modal.sale {
+        background: red;
+    }
+
+    .modal.sale h1 {
+        color: white;
+    }
+</style>
